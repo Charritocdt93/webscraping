3eaa19053a58ed2719ec24b72eda586e845ca8e8
David <davidcdt93@gmail.com>
2024-01-16 18:58:13 +0100

    cm1

---
+++ src/Services/visualizacion.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Paso 1: Abrir el archivo CSV con pandas
+archivo = '../data/noticias.csv'
+datos = pd.read_csv(archivo)
+
+# Paso 2: Visualizar los primeros registros para entender la estructura de los datos
+print(datos.head())
+
+# Paso 3: Crear un gráfico con Matplotlib
+plt.scatter(datos['categoria'], datos['fecha'])
+plt.title('Gráfico de dispersión de categorias vs fechas')
+plt.xlabel('categoria')
+plt.ylabel('fecha')
+
+# Mostrar el gráfico directamente en la co
